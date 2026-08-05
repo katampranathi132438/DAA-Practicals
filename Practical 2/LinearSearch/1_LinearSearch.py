b5cf2b27aa121edcabc0f5cbbefe45725ac61ab9
katam pranathi <katampranathi.132438@marwadiuniversity.ac.in>
2026-08-05 15:59:00 +0530

    LinearSearch

--- Practical 2/LinearSearch/1_LinearSearch.py
+++ Practical 2/LinearSearch/1_LinearSearch.py
@@ -1,15 +1,12 @@
 n = int(input("Enter the number of elements: "))
-
 arr = []
 print("Enter the elements:")
 for i in range(n):
     arr.append(int(input()))
-
 key = int(input("Enter the element to search: "))
-
 for i in range(n):
     if arr[i] == key:
         print("Element found at position", i + 1)
         break
 else:
-    print("Element not found")
+   print("Element not found")
