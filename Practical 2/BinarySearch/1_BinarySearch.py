da773c87d4421712b677e7fb451a5b696412172d
katam pranathi <katampranathi.132438@marwadiuniversity.ac.in>
2026-08-05 15:36:42 +0530

    BinarySearch

--- Practical 2/BinarySearch/1_BinarySearch.py
+++ Practical 2/BinarySearch/1_BinarySearch.py
@@ -1,19 +1,13 @@
-
 n = int(input("Enter the number of elements: "))
-
 arr = []
 print("Enter the elements in sorted order:")
 for i in range(n):
     arr.append(int(input()))
-
 key = int(input("Enter the element to search: "))
-
 low = 0
 high = n - 1
-
 while low <= high:
     mid = (low + high) // 2
-
     if arr[mid] == key:
         print("Element found at position", mid + 1)
         break
